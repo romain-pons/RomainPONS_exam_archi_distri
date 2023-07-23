0be5fa3aea6f2afef293ff49505dd9fb3a4a73bc
romain-pons <75258269+romain-pons@users.noreply.github.com>
2023-07-23 18:09:16 +0200

    Initial commit

---
+++ src/main/java/com/example/romainpons_exam_archi_distri/RomainPonsExamArchiDistriApplication.java
@@ -0,0 +1,13 @@
+package com.example.romainpons_exam_archi_distri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomainPonsExamArchiDistriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomainPonsExamArchiDistriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/romainpons_exam_archi_distri/RomainPonsExamArchiDistriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.romainpons_exam_archi_distri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomainPonsExamArchiDistriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
